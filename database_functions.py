09d27030a8a67a7b2c0b1761c74a8a6e93dd57e4
jdawidowicz <117271720+jdawidowicz@users.noreply.github.com>
2022-11-18 13:05:26 +0000

    All DB Functions written

--- database_functions.py
+++ database_functions.py
@@ -1,6 +1,11 @@
 import pymysql
 import os
+import string
 from dotenv import load_dotenv
+
+#make functions take/return dictionaries
+
+
 
 
 def connect():
@@ -15,10 +20,89 @@ def connect():
         host,
         user,
         password,
-        database
+        database,
+        cursorclass = pymysql.cursors.DictCursor
     )
     return connection
 
 def disconnect(cursor,connection):
     cursor.close()
     connection.close()
+
+def load_table(table): #returns list of dictionaries (lod)
+    connection = connect()
+    cursor = connection.cursor()
+    
+    cursor.execute(f'SELECT * FROM {table}')
+
+    rows = cursor.fetchall()
+    lod = []
+    for row in rows:
+        lod.append(row)
+    
+    disconnect(cursor,connection)
+    return lod
+
+def add_new_record(table, newdict):
+    connection = connect()
+    cursor = connection.cursor()
+
+    keys = newdict.keys()
+    keys = list(keys)
+    sqlstr=''
+    keystr = ''
+    valuestr = ''
+    
+    for key in keys:
+        try:
+            value = float(newdict[key])
+            valuestr += f',{value}'
+        except:
+            value = newdict[key]
+            valuestr+= f",'{value}'"
+        keystr += f',{key}'
+    valuestr = valuestr.lstrip(',')
+    keystr = keystr.lstrip(',')
+    sqlstr = (f'INSERT INTO {table} ({keystr}) VALUES ({valuestr})')
+    print(sqlstr)
+
+    cursor.execute(sqlstr)
+    connection.commit()
+    
+    disconnect(cursor,connection)
+
+def delete_record(table, id_number):
+    connection = connect()
+    cursor = connection.cursor()
+    tablestr = table.rstrip('s')
+
+
+    cursor.execute(f'DELETE FROM {table} WHERE {tablestr}_id = {id_number}')
+    connection.commit()
+    
+    disconnect(cursor,connection)
+
+def update_record(table, newdict):
+    connection = connect()
+    cursor = connection.cursor()
+
+    keys = list(newdict.keys())
+    id_number = keys[0]
+    del keys[0]
+
+    update_str=''
+    for key in keys:
+        try:
+            value = float(newdict[key])
+            update_str += f'{key} = {value},'
+        except:
+            value = newdict[key]
+            update_str+= f"{key} = '{value}',"
+    id_str = table.rstrip('s')
+    update_str = update_str.rstrip(',')
+    cursor.execute(f'UPDATE {table} SET {update_str} WHERE {id_str}_id = {id_number};')
+    
+
+add_new_record('products', {beans})
+print(load_table('products'))
+print(load_table('couriers'))
